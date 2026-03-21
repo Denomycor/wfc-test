28a56b37eefd559d9415f0d2b937f29d448869b0
Denomycor <afonsoresteves@hotmail.com>
2026-03-21 03:38:13 +0000

    Improvements

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,23 @@ using namespace wfc;
 
 int main(){
     srand(1);
-    wfc::WFC wfc({10,10,1}, {1,1,1});
-    wfc.get_constraints().change_rule(1, wfc::Directions::UP, 2, false);
-    std::cout << wfc.get_constraints().get()[0][1].size();
-    // wfc.get_constraints().change_rule(1, wfc::Directions::DOWN, 2, false);
-    // wfc.get_constraints().change_rule(1, wfc::Directions::LEFT, 2, false);
-    // wfc.get_constraints().change_rule(1, wfc::Directions::RIGHT, 2, false);
+    WFC wfc({10,10,1}, {1,1,1});
+    wfc.get_constraints().change_rule(1, Directions::UP, 2, false);
+    wfc.get_constraints().change_rule(1, Directions::DOWN, 2, false);
+    wfc.get_constraints().change_rule(1, Directions::LEFT, 2, false);
+    wfc.get_constraints().change_rule(1, Directions::RIGHT, 2, false);
     std::cout << wfc.get_constraints().get() << std::endl; 
 
-    wfc.stepped.connect([](wfc::WFC* ptr){
+    wfc.stepped.connect([](wfc::WFC* ptr, int c, Vec3u cell){
+        std::cout << "-------------------------------" << std::get<0>(cell) << " " << std::get<1>(cell) << std::endl;
         wfc::print_array2d(ptr->get_wave());
-        std::cout << "-------------------------------" << std::endl;
     });
 
-    wfc.finished.connect([](wfc::WFC* ptr){
-        wfc::print_array2d(ptr->get_result());
+    wfc.finished.connect([](WFC* ptr){
+        print_array2d(ptr->get_result());
     });
 
     wfc.init();
-    wfc::print_array2d(wfc.get_wave());
-    auto p = wfc.select_cell();
-    wfc.collapse_cell(p.value());
-    // wfc.propagate_constraints(p.value());
-    wfc::print_array2d(wfc.get_wave());
+    wfc.run();
 }
 
